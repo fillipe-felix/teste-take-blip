ba7cb0a2f298764210c2a27dfa518455d40400d3
fillipe-felix <felipesoares_1993@hotmail.com>
2021-04-29 15:54:35 -0300

    adicionar a url do avatar do repositorio do Git

--- src/main/java/com/takeblip/testetakeblip/api/entity/Owner.java
+++ src/main/java/com/takeblip/testetakeblip/api/entity/Owner.java
@@ -9,30 +9,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class TakeBlip implements Serializable {
+public class Owner implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-
-    private String description;
-
-    private String created_at;
-
-    private String language;
-
-    @OneToOne
-    private Owner owner;
+    private String avatar_url;
 }
